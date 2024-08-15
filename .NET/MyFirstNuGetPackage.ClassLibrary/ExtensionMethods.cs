af72c738de8f589d3d83b4fbc70ffe6623b7996b
Taner Saydam <83103843+TanerSaydam@users.noreply.github.com>
2024-08-15 11:39:38 +0300

    MIT license eklendi ve ilk Nuget package yazıldı

---
+++ .NET/MyFirstNuGetPackage.ClassLibrary/ExtensionMethods.cs
@@ -0,0 +1,11 @@
+﻿namespace MyFirstNuGetPackage.ClassLibrary;
+
+public static class ExtensionMethods
+{
+    public static string MyToUpperCase2(this string str)
+    {
+        string name = "Taner Saydam";
+        name.MyYoUpperCase();
+        return str.ToUpper();
+    }
+}
---
+++ .NET/MyFirstNuGetPackage.ClassLibrary/README.md
@@ -0,0 +1,10 @@
+# My Upper Case NuGet Package
+
+Bu eğitim amaçlı hazırlanmış bir pakettir.
+
+Örnek kullanım:
+```csharp
+string name = "taner";
+
+name.MyToUpperCase(); // response = TANER
+```
